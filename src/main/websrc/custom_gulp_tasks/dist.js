dde38a84b462162d6a77f3ae2eabacffe0ce57a9
dmadheswaran <deepa.madheswaran@edrington.com>
2016-11-15 11:42:03 +0000

    added in dist.js

---
+++ src/main/websrc/custom_gulp_tasks/dist.js
@@ -0,0 +1,29 @@
+/* -------------------------------------------------------------------------------------------------
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+Dist - Gulp Processes
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+------------------------------------------------------------------------------------------------- */
+
+// Load gulp module
+
+var gulp = require('gulp');
+var gulpif = require('gulp-if');
+var config = require('../node_modules/ss-build-gulp/config');
+
+// Prism JS
+//--------------------------------
+
+var condition = true;
+
+if (config.name === 'STYLEGUIDE' || config.name === 'DEV') {
+    condition = false;
+}
+
+gulp.task('dist', function() {
+    return gulp.src([
+            config.paths.dist + '/**/*'
+        ])
+        .pipe(gulpif(condition, gulp.dest(config.paths.target + '/webapp')));
+});
